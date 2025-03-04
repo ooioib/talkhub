05419b9e349753058a39c15475c377b24b7959e4
rin <krin02@naver.com>
2025-03-04 16:01:09 +0900

    feat : 회원 가입 기능 구현완료

--- src/main/java/org/codenova/talkhub/model/dao/UserDAO.java
+++ src/main/java/org/codenova/talkhub/model/dao/UserDAO.java
@@ -15,7 +15,7 @@ public class UserDAO {
         boolean result = false;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://database.ch662qumapvg.ap-northeast-2.rds.amazonaws.com:3306/talkhub",
+            Connection conn = DriverManager.getConnection("jdbc:mysql://database.cpmkaio4y6nw.ap-northeast-2.rds.amazonaws.com/talkhub",
                     "admin", "1q2w3e4r");
 
             PreparedStatement ps = conn.prepareStatement("insert into users values(?, ?, ?, ?, ?, now())");
